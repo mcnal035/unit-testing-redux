f98a5d00ad7fc8145a2325225c3793c734191766
Mark McNally <mcnal1987@gmail.com>
2019-08-19 16:54:37 -0500

    fixed the error.

--- test/counter.reducer.test.js
+++ test/counter.reducer.test.js
@@ -26,10 +26,18 @@ test('counterReducer should initally be 0', () => {
 
 // counterReducer should be able to ADD & SUBTRACT (two actions)
 test('counterReducer should initally be 0', () => {
-  const action = {type: 'SUBTRACT', type:'ADD'};
+  const subaction = {type: 'SUBTRACT'};
+  const addaction = {type:'ADD'}
+  // First argument is the existing state, the second is your action
+  const addState = counterReducer(undefined, addaction);
+  const subtractState = counterReducer(addState, subaction);
+  expect(subtractState).toBe(0);
+});
+
+// counterReducer should ignore actions it doesn't care about IGNORE_ME
+test('counterReducer should initally be 0', () => {
+  const action = {type: 'IGNORE_ME', type:'ADD'};
   // First argument is the existing state, the second is your action
   const returnedState = counterReducer(undefined, action);
   expect(returnedState).toBe(1);
 });
-
-// counterReducer should ignore actions it doesn't care about IGNORE_ME
